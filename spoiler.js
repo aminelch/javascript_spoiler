2c98eec9737ee9f0f00b4d8e653695e5267c0df2
Amine L'ch <aminelch95@gmail.com>
2017-11-09 00:01:51 +0100

    first commit

---
+++ spoiler.js
@@ -0,0 +1,54 @@
+/* ON SELECTION  l'element qui a class="spoiler"
+ ON STOCKE son contenu texte
+ ON MASKE  le texte dedans 
+    ON CRÉ UN element pourtant la classe .spoiler-content
+    ON CRÉ un bUTTON qui affiche le spoiler 
+*/
+
+
+//crée un button avec le contenu Afficher le spoiler et 
+//l'ajoute au DOM 
+var createButton = function(element) {
+  //on cré le button spoiler
+  var button = document.createElement("button");
+  button.className = "spoiler-button";
+  button.textContent = "Afficher le spoiler";
+  //on ajoute le button avant element
+
+  element.parentElement.insertBefore(button, element);
+  //on cache le span mais elle reste encore EXISTE au DOM 
+  hideElement(element)
+};
+
+//on ajoute l'ecouteur chargement de document
+document.addEventListener("DOMContentLoaded", function() {
+  //on selectionnes tt span ont la class=.spoiler
+  var spoilers = document.querySelectorAll(".spoiler");
+  for (var i = 0; i < spoilers.length; i++) {
+    createButton(spoilers[i]);
+   
+  }
+
+
+
+
+
+
+//cache un element 
+var hideElement=function (element){
+  element.style.display="none"
+}
+
+
+
+
+  var btns= document.querySelectorAll("button .spoiler-button")
+  
+   btns[i].addEventListener("click",function(){
+    console("click")
+    
+   })
+  
+  
+});
+
